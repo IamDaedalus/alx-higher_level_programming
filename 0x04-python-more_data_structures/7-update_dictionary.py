0cb034d0c1d5907d47cb980e97b9179604525864
Daedalus <manny.is.daedalus@pm.me>
2023-08-29 23:33:04 +0000

    added null checks

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    if len(a_dictionary) == 0:
+        return
+
     a_dictionary[key] = value
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
+    if len(a_dictionary) == 0:
+        return
+
     if key in a_dictionary:
         del a_dictionary[key]
